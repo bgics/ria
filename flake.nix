087aed34cd37deed07c091804351bbbefa4ae79b
Bhuvansh Goyal <bhuvansh.iitb@gmail.com>
2025-07-15 00:15:00 +0530

    init

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "rust development flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cargo
+              rustc
+              rustfmt
+              rust-analyzer
+              taplo
+            ];
+            
+            shellHook = ''
+              export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+            '';
+          };
+        };
+      }
+    );
+}
